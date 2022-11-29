c156b609901dcb3ba17ea48e9ca7c362a6cd3531
haewon <30405705+wooheet@users.noreply.github.com>
2022-11-30 00:56:53 +0900

    Add file name sort

--- python/exams/kakao/kakao_file_name_sort.py
+++ python/exams/kakao/kakao_file_name_sort.py
@@ -2,8 +2,6 @@ import re
 
 
 def solution(files):
-    answer = []
-
     find_num = re.compile(r'\d+')
     space = re.compile('[ \t\r\n\v\f]')
 
@@ -12,29 +10,13 @@ def solution(files):
     for i, f in enumerate(files):
         num = find_num.findall(f)
         split_num = f.split(num[0])
-        temp[(split_num[0], int(num[0]), i)] = f
-    print(temp)
+        temp[(split_num[0].upper(), float(num[0]) + float(i/10))] = f
 
-    # head 먼저 정렬
-    # 숫자 정렬
-    # for t in temp:
-    #     print(t[0])
-    #     print(t[1])
-    #     # print(temp[t])
+    sort_dic = dict(sorted(temp.items(), key=lambda item: item[0]))
 
-    # for i in sorted(files):
-    #     num = find_num.findall(i)
-    #     temp[num[0]] = i
-    #
-    # x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
-    #
-    # print(dict(sorted(x.items(), key=lambda item: item[1])))
-    # {k: v for k, v in sorted(x.items(), key=lambda item: item[1])}
-
-
-    return answer
+    return [sort_dic[i] for i in sort_dic]
 
 
 files = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
 # ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
-solution(files)
+print(solution(files))
