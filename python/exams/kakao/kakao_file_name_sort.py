058536c9b9fc61fb182c45c7a33a348170fcbb85
haewon <30405705+wooheet@users.noreply.github.com>
2022-11-30 01:00:16 +0900

    Add file name sort

--- python/exams/kakao/kakao_file_name_sort.py
+++ python/exams/kakao/kakao_file_name_sort.py
@@ -17,6 +17,16 @@ def solution(files):
     return [sort_dic[i] for i in sort_dic]
 
 
+def solution2(files):
+    temp = [re.split(r"([0-9]+)", s) for s in files]
+
+    sort = sorted(temp, key = lambda x: (x[0].lower(), int(x[1])))
+
+    return [''.join(s) for s in sort]
+
 files = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
+# files = ["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]
 # ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
+# ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
 print(solution(files))
+print(solution2(files))
