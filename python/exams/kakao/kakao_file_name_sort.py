64c6c95d8dff52c8f12cdd802b8a998b27120bdd
haewon <30405705+wooheet@users.noreply.github.com>
2022-11-30 01:07:11 +0900

    Add just now song

--- python/exams/kakao/kakao_file_name_sort.py
+++ python/exams/kakao/kakao_file_name_sort.py
@@ -24,9 +24,17 @@ def solution2(files):
 
     return [''.join(s) for s in sort]
 
+
+def solution3(files):
+    a = sorted(files, key=lambda file : int(re.findall('\d+', file)[0]))
+    print(a)
+    b = sorted(a, key=lambda file : re.split('\d+', file.lower())[0])
+    return b
+
 files = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
 # files = ["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]
 # ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
 # ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
-print(solution(files))
-print(solution2(files))
+# print(solution(files))
+# print(solution2(files))
+print(solution3(files))
