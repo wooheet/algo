651e82ae11e4536aa8f9bfeeba6bae112088e90e
haewon <n2203020@N2203020ui-MacBookPro.local>
2022-11-30 18:24:34 +0900

    Add friends block

---
+++ python/exams/kakao/kakao_friends_block.py
@@ -0,0 +1,51 @@
+def solution(m, n, board):
+    x = []
+
+    for i in board:
+        x.append(list(i))
+    print(x)
+
+    point = 1
+    while point != 0:
+        x_list = []
+        point = 0
+
+        for i in range(m - 1):
+            for j in range(n - 1):
+
+                if x[i][j] == x[i][j + 1] == x[i + 1][j] == x[i + 1][j + 1] != '팡!':
+                    x_list.append([i, j])
+                    point += 1
+
+        print(x_list)
+
+        for i2 in x_list:
+            i, j = i2[0], i2[1]
+            x[i][j], x[i][j + 1], x[i + 1][j], x[i + 1][j + 1] = '팡!', '팡!', '팡!', '팡!'
+
+        for i3 in range(m):
+            for i in range(m - 1):
+                for j in range(n):
+                    if x[i + 1][j] == '팡!':
+                        x[i + 1][j], x[i][j] = x[i][j], '팡!'
+
+    cnt = 0
+    for i in x:
+        cnt += i.count('팡!')
+    return cnt
+
+
+# CCBDE
+# AAADE
+# AAABF
+# CCBBF
+
+
+# 아래 리스트가 공백이면 현재 위치에 있는 배열 컬럼 위치에서 빈 위치로 옮겨준다.
+
+
+m = 4
+n = 5
+board = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
+
+solution(m,n,board)
--- python/exams/kakao/kakao_news_clustering.py
+++ python/exams/kakao/kakao_news_clustering.py
@@ -1,6 +1,7 @@
 # isdisjoint() - 두 집합이 공통 원소를 갖지 않는가?
 # issubset() - 부분집합(subset)인가?
 # issuperset() - 확대집합(superset)인가?
+
 # union() - 합집합을 만들어 리턴
 # update() - 합집합을 만들어 원본 데이터를 갱신(수정)
 # difference() - 차집합을 만들어 리턴
@@ -9,27 +10,61 @@
 
 import re
 
+
 def solution(str1, str2):
-    p = re.compile('[a-z]+')
-
-    answer = 0
-
     l_str1 = list(str1)
     l_str2 = list(str2)
 
-    tmp_str1 = list()
-    tmp_str2 = list()
+    r_str1 = make(l_str1)
+    r_str2 = make(l_str2)
 
-    # 각각 {FR, RA, AN, NC, CE}, {FR, RE, EN, NC, CH}가 되며, 교집합은 {FR, NC}, 합집합은 {FR, RA, AN, NC, CE, RE, EN, CH}
+    print(r_str1)
+    print(r_str2)
 
-    for i in range(len(l_str1)):
-        for i in l_str2:
-            if len(p.findall(i)):
+    s_s1 = set(r_str1)
+    s_s2 = set(r_str2)
 
-                tmp_str1.append()
+    inter_section = s_s1.intersection(s_s2)
+    union = s_s1.union(s_s2)
+
+    jacard = len(inter_section) / len(union)
+
+    if 0 < jacard or 1 > jacard:
+        return 65536
+
+    answer = jacard * 65536
+
+    return int(answer)
 
 
-    return answer
+def make(source):
+    p = re.compile('[a-zA-Z]+')
+
+    make_str_list = list()
+
+    for i in range(len(source)):
+        if len(p.findall(source[i])):
+            item = source[i:i + 2]
+            join_str = ''.join((item)).upper()
+
+            if len(join_str.strip()) == 2:
+                make_str_list.append(join_str)
+
+    return make_str_list
 
 
-solution('FRANCE', '=french')
+# str1 = 'FRANCE'
+# str2 = 'french'
+
+str1 = "handshake"
+str2 = "shake hands"
+
+# str1 = '123'
+# str2 = '234'
+
+print(solution(str1, str2))
+
+
+
+
+
